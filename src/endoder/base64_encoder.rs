840a873d21f7cb0318f50a66f2f68fe83a885575
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-07-07 16:09:23 -0700

    Just having fun

--- src/endoder/base64_encoder.rs
+++ src/endoder/base64_encoder.rs
@@ -1,3 +1,5 @@
+#[allow(unused)]
+// pub here means it's public to the module it's defined in, not the whole project
 pub fn encode() {
     println!("encoding")
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,66 @@
-// this step is what allows stuff to be visible
-mod audio;
-mod endoder;
-
-// this is where we specify what we actually wanna use
-use audio::whoofer::something;
+use std::fmt::format;
 
 fn main() {
-    let message: &str = "The name is Nnaemeka Onyeokoro";
-    something();
-    println!("{message}")
+    let mut colors = ["white", "green", "blue", "purple"];
+    println!("Before updating, colors is: {:?}", colors);
+    update_colors(&mut colors[2..4]);
+    println!("Colors is now: {:?}", colors);
+
+    // using tuples
+    let person_details: (&str, bool, i16) = ("Nnaemeka", true, 24);
+
+    // using structs
+    let mut emeka = Person {
+        first_name: String::from("Nnaemeka"),
+        last_name: String::from("Onyeokoro"),
+        age: 23,
+        employed: (true, String::from("Google")),
+    };
+
+    if emeka.employed.0 {
+        println!("Emeka is employed at: {}", emeka.employed.1);
+    } else {
+        println!("Emeka is about to be employed at Google as a Rust engineer.");
+    }
+
+    emeka.display_details();
+    println!("{:?}", emeka);
+
+    emeka.update_first_name("kamsi");
+    emeka.display_details();
+
+    // invoke static method of the Person Struct
+    Person::say_hello();
+}
+
+// for slices
+fn update_colors(color_slice: &mut [&str]) {
+    println!("Updating colors now...");
+    color_slice[0] = "MAGENTA";
+    color_slice[1] = "CYAN";
+}
+
+// structures
+#[derive(Debug)]
+#[allow(unused)]
+struct Person {
+    first_name: String,
+    last_name: String,
+    age: u32,
+    employed: (bool, String), // if employed, then where
+}
+
+impl Person {
+    fn display_details(&self) {
+        let message = format!("{} works at {}", self.first_name, self.employed.1);
+        println!("{}", message);
+    }
+
+    fn update_first_name(&mut self, new_name: &str) {
+        self.first_name = new_name.to_string();
+    }
+
+    fn say_hello() {
+        println!("I am a Person");
+    }
 }
