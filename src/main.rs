b332bd856a7b2df9cacec7101c2a6cf9b04cf788
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-07-11 01:26:16 -0700

    Rust is interesting

--- src/main.rs
+++ src/main.rs
@@ -1,115 +1,29 @@
-use crate::utils::closures::apply;
-
-mod utils;
-
 #[allow(unused)]
 fn main() {
-    let times_two = |x: &mut i32| *x * 2;
-    let mut a: i32 = 10;
-    println!("{}", apply(times_two, &mut a));
+    let mat1 = vec![
+        vec![1, 2, 3, 4],
+        vec![1, 2, 3, 4],
+        vec![1, 2, 3, 4],
+        vec![1, 2, 3, 4],
+    ];
+    let mat2 = vec![vec![8, 2, 3], vec![1, 2, 3], vec![1, 2, 3]];
+    let mat3 = vec![
+        vec![1, 2, 3, 4, 9],
+        vec![1, 2, 3, 4, 0],
+        vec![1, 2, 3, 4, 4],
+        vec![1, 2, 3, 4, 9],
+        vec![1, 2, 3, 4, 2],
+    ];
+    let mat4 = vec![vec![9, 4], vec![1, -2]];
+    println!("{}", matrix_diag_sum(&mat4));
+}
 
-    let mut colors = ["white", "green", "blue", "purple"];
-    println!("Before updating, colors is: {:?}", colors);
-    update_colors(&mut colors[2..4]);
-    println!("Colors is now: {:?}", colors);
-
-    // using tuples
-    let person_details: (&str, bool, i16) = ("Nnaemeka", true, 24);
-
-    // using structs
-    let mut emeka = Person {
-        first_name: String::from("Nnaemeka"),
-        last_name: String::from("Onyeokoro"),
-        age: 23,
-        employed: (true, String::from("Google")),
-    };
-
-    if emeka.employed.0 {
-        println!("Emeka is employed at: {}", emeka.employed.1);
-    } else {
-        println!("Emeka is about to be employed at Google as a Rust engineer.");
+fn matrix_diag_sum(mat: &Vec<Vec<i32>>) -> i32 {
+    let tt = &mat[1];
+    let mut sum: i32 = 0;
+    // square matrix
+    for i in 0..mat.len() {
+        sum += mat[i][i];
     }
-
-    emeka.display_details();
-    println!("{:?}", emeka);
-
-    emeka.update_first_name("kamsi");
-    emeka.display_details();
-
-    // invoke static method of the Person Struct
-    Person::say_hello();
-
-    // using the Direction Enum
-    let north = Direction::NORTH;
-    let west = Direction::WEST;
-
-    spit_coordinates(&north);
-    spit_coordinates(&west);
-
-    // closure
-    let mut count: i32 = 0;
-}
-
-// for slices
-fn update_colors(color_slice: &mut [&str]) {
-    println!("Updating colors now...");
-    color_slice[0] = "MAGENTA";
-    color_slice[1] = "CYAN";
-}
-
-// structures
-#[derive(Debug)]
-#[allow(unused)]
-struct Person {
-    first_name: String,
-    last_name: String,
-    age: u32,
-    employed: (bool, String), // if employed, then where
-}
-
-impl Person {
-    fn display_details(&self) {
-        let message = format!("{} works at {}", self.first_name, self.employed.1);
-        println!("{}", message);
-    }
-
-    fn update_first_name(&mut self, new_name: &str) {
-        self.first_name = new_name.to_string();
-    }
-
-    fn say_hello() {
-        println!("I am a Person");
-    }
-}
-
-// Traits are like interfaces. Define with `trait` keyword:
-trait Greet {
-    fn greet(&self);
-}
-
-fn spit_coordinates(direction: &Direction) {
-    // this is how u do switch equivalent in Rust
-    match direction {
-        Direction::EAST => println!("Going towards the East!"),
-        Direction::NORTH => println!("Going towards the North"),
-        Direction::SOUTH => println!("Going towards the SOUTH"),
-        Direction::WEST => println!("Going towards the WEST"),
-    }
-}
-
-#[derive(Debug)]
-#[allow(unused)]
-enum Direction {
-    NORTH,
-    SOUTH,
-    WEST,
-    EAST,
-}
-
-#[derive(Debug)]
-#[allow(unused)]
-// generics
-struct Point<T> {
-    x: T,
-    y: T,
+    sum
 }
